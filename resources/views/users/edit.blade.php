b89b048300826b23ba630c2429dd3d76db7a4357
thuatnt2 <thuatnt2@gmail.com>
2016-04-19 16:21:00 +0000

    change password

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,14 +8,14 @@
 <div class="row">
     <div class="box row-form">
         {!! Former::setOption('TwitterBootstrap3.labelWidths', ['large' => 4, 'small' => 4]) !!}
-        {!! Former::horizontal_open(action('UnitController@update', $user->id))->id('form-changepassword') !!}
+        {!! Former::horizontal_open(action('UserController@update', $user->id))->id('form-changepassword') !!}
         {{ method_field('PUT') }}
         <fieldset>
         {!! Former::legend('Form đổi mật khẩu') !!}
         <div class="col-sm-offset-3 col-sm-4">
-            {!! Former::password('oldpassword', 'Mật khẩu cũ')->required()->addClass('input-sm'); !!}
-            {!! Former::password('newpassword', 'Mật khẩu mới')->required()->addClass('input-sm'); !!}
-            {!! Former::password('confirmpassword', 'Xác nhận')->required()->addClass('input-sm'); !!}
+            {!! Former::password('now_password', 'Mật khẩu cũ')->required()->addClass('input-sm'); !!}
+            {!! Former::password('password', 'Mật khẩu mới')->required()->addClass('input-sm'); !!}
+            {!! Former::password('password_confirmation', 'Xác nhận')->required()->addClass('input-sm'); !!}
             <div class="form-group">
                 <div class="col-lg-offset-4 col-sm-offset-4 col-lg-8 col-sm-8">
                      <button type="submit" class="btn btn-success btn-small"><i class="fa fa-check">&nbsp</i>Đồng ý</button>
