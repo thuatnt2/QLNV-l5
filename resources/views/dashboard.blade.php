ca2ece01984c03112ad2a682065a4dd996665b54
thuatnt2 <thuatnt2@gmail.com>
2016-04-20 02:49:48 +0000

    rm file

--- resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-@section('css')
-<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-@stop
-@section('content')
-@stop
-@section('javascript')
-<script src="{{ URL::asset('js/app.js') }}"></script>
-@stop
