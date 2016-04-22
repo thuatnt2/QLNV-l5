e5c6d7238b48ba979f433ac16169fd934a39f160
thuatnt2 <thuatnt2@gmail.com>
2016-04-22 16:48:18 +0000

    sdfsd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use App\User;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -26,6 +26,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $users = User::all();
+        return view('home', compact('users', 'managers'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,20 @@
 <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 @stop
 @section('content')
+<div class="row">
+	<div class="box">
+		 <div class="box-header text-center">
+            <h3 class="box-title" style="color: red; font-weight: bold;">PHÂN CÔNG GIÁM SÁT</h3>
+        </div>
+		<div class="box-body">
+			<table id="example1" class="table table-bordered">
+				@foreach ($users as $user)
+					<th>{{ $user->fullname }}</th>
+				@endforeach
+			</table>
+		</div>
+	</div>
+</div>
 @stop
 @section('javascript')
 <script src="{{ URL::asset('js/app.js') }}"></script>
